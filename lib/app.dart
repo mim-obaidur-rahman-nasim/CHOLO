70415d867afddbda1cf1c6ab798d796a3ce1bddc
Mim Obaidur Rahman Nasim <obaidur150@gmail.com>
2025-12-23 23:40:26 +0200

    CHolo clean project setup with folder structure

---
+++ lib/app.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'CHolo',
+      debugShowCheckedModeBanner: false,
+      home: const Scaffold(
+        body: Center(
+          child: Text(
+            'CHolo',
+            style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+          ),
+        ),
+      ),
+    );
+  }
+}
